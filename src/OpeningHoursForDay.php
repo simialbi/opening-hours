dd55efcadad9dee51ef7ec2114b63765b7e14c9d
simialbi <simi.albi@outlook.com>
2019-01-21 09:35:15 +0100

    added php doc changed implode character updated code style

--- src/OpeningHoursForDay.php
+++ src/OpeningHoursForDay.php
@@ -2,14 +2,14 @@
 
 namespace Spatie\OpeningHours;
 
-use Countable;
 use ArrayAccess;
 use ArrayIterator;
+use Countable;
 use IteratorAggregate;
-use Spatie\OpeningHours\Helpers\Arr;
-use Spatie\OpeningHours\Helpers\DataTrait;
 use Spatie\OpeningHours\Exceptions\NonMutableOffsets;
 use Spatie\OpeningHours\Exceptions\OverlappingTimeRanges;
+use Spatie\OpeningHours\Helpers\Arr;
+use Spatie\OpeningHours\Helpers\DataTrait;
 
 class OpeningHoursForDay implements ArrayAccess, Countable, IteratorAggregate
 {
@@ -18,6 +18,10 @@ class OpeningHoursForDay implements ArrayAccess, Countable, IteratorAggregate
     /** @var \Spatie\OpeningHours\TimeRange[] */
     protected $openingHours = [];
 
+    /**
+     * @param array $strings
+     * @return OpeningHoursForDay
+     */
     public static function fromStrings(array $strings)
     {
         if (isset($strings['hours'])) {
@@ -42,6 +46,10 @@ class OpeningHoursForDay implements ArrayAccess, Countable, IteratorAggregate
         return $openingHoursForDay;
     }
 
+    /**
+     * @param Time $time
+     * @return bool
+     */
     public function isOpenAt(Time $time)
     {
         foreach ($this->openingHours as $timeRange) {
@@ -53,6 +61,11 @@ class OpeningHoursForDay implements ArrayAccess, Countable, IteratorAggregate
         return false;
     }
 
+    /**
+     * @param Time $time
+     * @return bool|mixed|Time
+     * @throws Exceptions\InvalidTimeRangeString
+     */
     public function nextOpen(Time $time)
     {
         foreach ($this->openingHours as $timeRange) {
@@ -72,6 +85,11 @@ class OpeningHoursForDay implements ArrayAccess, Countable, IteratorAggregate
         return false;
     }
 
+    /**
+     * @param Time $time
+     * @return bool|mixed|Time
+     * @throws Exceptions\InvalidTimeRangeString
+     */
     public function nextClose(Time $time)
     {
         foreach ($this->openingHours as $timeRange) {
@@ -91,102 +109,171 @@ class OpeningHoursForDay implements ArrayAccess, Countable, IteratorAggregate
         return false;
     }
 
+    /**
+     * @param Time $time
+     * @param TimeRange $timeRange
+     * @return mixed
+     */
     protected function findNextOpenInWorkingHours(Time $time, TimeRange $timeRange)
     {
         if ($timeRange->containsTime($time) && next($timeRange) !== $timeRange) {
             return next($timeRange);
         }
+
+        return null;
     }
 
+    /**
+     * @param Time $time
+     * @param TimeRange $timeRange
+     * @param TimeRange|null $prevTimeRange
+     * @return Time
+     * @throws Exceptions\InvalidTimeRangeString
+     */
     protected function findNextOpenInFreeTime(Time $time, TimeRange $timeRange, TimeRange &$prevTimeRange = null)
     {
         $timeOffRange = $prevTimeRange ?
-            TimeRange::fromString($prevTimeRange->end().'-'.$timeRange->start()) :
-            TimeRange::fromString('00:00-'.$timeRange->start());
+            TimeRange::fromString($prevTimeRange->end() . '-' . $timeRange->start()) :
+            TimeRange::fromString('00:00-' . $timeRange->start());
 
         if ($timeOffRange->containsTime($time) || $timeOffRange->start()->isSame($time)) {
             return $timeRange->start();
         }
 
         $prevTimeRange = $timeRange;
+
+        return null;
     }
 
+    /**
+     * @param Time $time
+     * @param TimeRange $timeRange
+     * @return mixed
+     */
     protected function findNextCloseInWorkingHours(Time $time, TimeRange $timeRange)
     {
         if ($timeRange->containsTime($time)) {
             return next($timeRange);
         }
+
+        return null;
     }
 
+    /**
+     * @param Time $time
+     * @param TimeRange $timeRange
+     * @param TimeRange|null $prevTimeRange
+     * @return Time
+     * @throws Exceptions\InvalidTimeRangeString
+     */
     protected function findNextCloseInFreeTime(Time $time, TimeRange $timeRange, TimeRange &$prevTimeRange = null)
     {
         $timeOffRange = $prevTimeRange ?
-            TimeRange::fromString($prevTimeRange->end().'-'.$timeRange->start()) :
-            TimeRange::fromString('00:00-'.$timeRange->start());
+            TimeRange::fromString($prevTimeRange->end() . '-' . $timeRange->start()) :
+            TimeRange::fromString('00:00-' . $timeRange->start());
 
         if ($timeOffRange->containsTime($time) || $timeOffRange->start()->isSame($time)) {
             return $timeRange->end();
         }
 
         $prevTimeRange = $timeRange;
+
+        return null;
     }
 
+    /**
+     * @param mixed $offset
+     * @return bool
+     */
     public function offsetExists($offset)
     {
         return isset($this->openingHours[$offset]);
     }
 
+    /**
+     * @param mixed $offset
+     * @return mixed|TimeRange
+     */
     public function offsetGet($offset)
     {
         return $this->openingHours[$offset];
     }
 
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @throws NonMutableOffsets
+     */
     public function offsetSet($offset, $value)
     {
         throw NonMutableOffsets::forClass(static::class);
     }
 
+    /**
+     * @param mixed $offset
+     */
     public function offsetUnset($offset)
     {
         unset($this->openingHours[$offset]);
     }
 
+    /**
+     * @return int
+     */
     public function count()
     {
         return count($this->openingHours);
     }
 
+    /**
+     * @return ArrayIterator|\Traversable
+     */
     public function getIterator()
     {
         return new ArrayIterator($this->openingHours);
     }
 
+    /**
+     * @return bool
+     */
     public function isEmpty()
     {
         return empty($this->openingHours);
     }
 
+    /**
+     * @param callable $callback
+     * @return array
+     */
     public function map(callable $callback)
     {
         return Arr::map($this->openingHours, $callback);
     }
 
+    /**
+     * @param array $openingHours
+     * @throws OverlappingTimeRanges
+     */
     protected function guardAgainstTimeRangeOverlaps(array $openingHours)
     {
         foreach (Arr::createUniquePairs($openingHours) as $timeRanges) {
+            /* @var $timeRanges TimeRange[] */
             if ($timeRanges[0]->overlaps($timeRanges[1])) {
                 throw OverlappingTimeRanges::forRanges($timeRanges[0], $timeRanges[1]);
             }
         }
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         $values = [];
         foreach ($this->openingHours as $openingHour) {
-            $values[] = (string) $openingHour;
+            $values[] = (string)$openingHour;
         }
 
-        return implode(',', $values);
+        return implode(' / ', $values);
     }
 }
